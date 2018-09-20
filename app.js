4fd64ec324dd77b48a89f9d081e99f8d7af35161
Ivan <ivanvlora@gmail.com>
2018-09-20 14:51:39 +0900

    Added express router and the ideas and user login routes

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Load Routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Connects to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+    useNewUrlParser: true
+})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
 
 const port = process.env.PORT || 3000;
 
@@ -9,17 +26,47 @@ const port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Method Override Middleware
+app.use(methodOverride('_method'));
+
+// Session Middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Flash Middleware
+app.use(flash());
+
+// Global Variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
 // Index Route
 app.get('/', (req, res) => {
     const title = "Welcome";
     res.render('index', {
-        title: title
+        title
     });
 });
 
+// About Route
 app.get('/about', (req, res) => {
     res.render('about');
 });
+
+// Use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 app.listen(port, () => {
     console.log(`Server Running on Port ${port}`);
