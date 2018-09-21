542cd345ebe7cae9b516d33a06dc6b3a2fe54cdb
Ivan <ivanvlora@gmail.com>
2018-09-21 09:47:57 +0900

    made a change to database file

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: process.env.PRODUCTION_DB}
+    module.exports = {mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds211083.mlab.com:11083/vidjot-prod`}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
